48bdb8a9a244c08b0ee123544f06890ffd3c2b07
Yihui Guo (Inspur) <v-yihguo@microsoft.com>
2017-07-24 22:50:02 +0800

    Move some magic numbers into appsetting.

---
+++ ReportGenerator/Provider/IConfigurationProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportGenerator.Provider
+{
+    public interface IConfigurationProvider
+    {
+        string GetConfigurationValue(string key);
+    }
+}
---
+++ ReportGenerator/Provider/LocalConfigurationProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportGenerator.Provider
+{
+    public class LocalConfigurationProvider:IConfigurationProvider
+    {
+        public string GetConfigurationValue(string key)
+        {
+            return ConfigurationManager.AppSettings[key] ?? "";
+        } 
+    }
+}
